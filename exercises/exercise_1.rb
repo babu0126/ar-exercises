caaaa766680af0998fb2e51d6f0d9fbba0473ad9
David Wang <18davidwang@gmail.com>
2022-12-12 23:47:51 -0800

    Finish exercise_1

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -4,3 +4,23 @@ puts "Exercise 1"
 puts "----------"
 
 # Your code goes below here ...
+Store.create(
+  name: "Burnaby",
+  annual_revenue: 300000,
+  mens_apparel: true,
+  women_apparel: true
+)
+Store.create(
+  name: "Richmond",
+  annual_revenue: 1260000,
+  mens_apparel: false,
+  women_apparel: true
+)
+Store.create(
+  name: "Gastown",
+  annual_revenue: 190000,
+  mens_apparel: true,
+  women_apparel: false
+)
+
+puts Store.count
